7e877d8a591334740fd0a31cbb892641b85c9dd8
Connor Duncan <connorduncan@cs.ucsb.edu>
2023-02-28 18:56:01 -0800

    Change a single character to lowercase

--- testbench.asm
+++ testbench.asm
@@ -9,6 +9,11 @@ main:
 	move $a0, $t1
 	li $v0, 11 # 11 is syscall code for printing character
 	syscall
+
+	# change the character to lowercase
+	addiu $t2, $t1, 32
+	move $a0, $t2
+	syscall
 	
 exit:
 	li $v0, 10
